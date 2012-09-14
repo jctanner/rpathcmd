3de0d3841629fe7be7d3b02163f11c67be485b37
jctanner <tanner.jc@gmail.com>
2012-09-14 16:19:35 -0400

    packages_test

--- packages.py
+++ packages.py
@@ -171,7 +171,7 @@ def do_package_spfimport(self, args):
     session_Token = package_factory_rsp [1][0]
     factoryHandle = package_factory_rsp[1][1][0][0]
 
-    epdb.st()
+    #epdb.st()
 
     # DO NOT UNDERSTAND THIS ... not sure if necessary
     #packageCreatorRecipeRsp = xmlrpcclient.get_package_creator_recipe(username,password,session_Token)
